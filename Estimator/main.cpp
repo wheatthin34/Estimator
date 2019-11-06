757b6d165f28d46318293623c079643704e8d402
Matt Wheatley <matthew.wheatley@snhu.edu>
2019-11-05 21:13:15 -0500

    Initial Commit

---
+++ Estimator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Estimator
+//
+//  Created by Matt Wheatley on 11/5/19.
+//  Copyright © 2019 Matt Wheatley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
